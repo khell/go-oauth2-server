820c83f249f46fd71c2e7b9f16a42e96a3816128
Cameron Yan <cameron.yan@cooltrax.com>
2018-10-23 15:22:54 +1100

    Add mssql support

--- database/database.go
+++ database/database.go
@@ -8,6 +8,7 @@ import (
 	"github.com/jinzhu/gorm"
 
 	// Drivers
+	_ "github.com/jinzhu/gorm/dialects/mssql"
 	_ "github.com/lib/pq"
 )
 
@@ -20,11 +21,12 @@ func init() {
 // NewDatabase returns a gorm.DB struct, gorm.DB.DB() returns a database handle
 // see http://golang.org/pkg/database/sql/#DB
 func NewDatabase(cnf *config.Config) (*gorm.DB, error) {
-	// Postgres
-	if cnf.Database.Type == "postgres" {
-		// Connection args
-		// see https://godoc.org/github.com/lib/pq#hdr-Connection_String_Parameters
-		args := fmt.Sprintf(
+	// Connection args
+	// see https://godoc.org/github.com/lib/pq#hdr-Connection_String_Parameters
+	var args string
+	switch cnf.Database.Type {
+	case "postgres":
+		args = fmt.Sprintf(
 			"sslmode=disable host=%s port=%d user=%s password='%s' dbname=%s",
 			cnf.Database.Host,
 			cnf.Database.Port,
@@ -32,24 +34,35 @@ func NewDatabase(cnf *config.Config) (*gorm.DB, error) {
 			cnf.Database.Password,
 			cnf.Database.DatabaseName,
 		)
-
-		db, err := gorm.Open(cnf.Database.Type, args)
-		if err != nil {
-			return db, err
-		}
-
-		// Max idle connections
-		db.DB().SetMaxIdleConns(cnf.Database.MaxIdleConns)
-
-		// Max open connections
-		db.DB().SetMaxOpenConns(cnf.Database.MaxOpenConns)
-
-		// Database logging
-		db.LogMode(cnf.IsDevelopment)
-
-		return db, nil
+	case "mssql":
+		args = fmt.Sprintf(
+			"sqlserver://%s:%s@%s:%d?database=%s",
+			cnf.Database.User,
+			cnf.Database.Password,
+			cnf.Database.Host,
+			cnf.Database.Port,
+			cnf.Database.DatabaseName,
+		)
 	}
 
-	// Database type not supported
-	return nil, fmt.Errorf("Database type %s not suppported", cnf.Database.Type)
+	if args == "" {
+		// Database type not supported
+		return nil, fmt.Errorf("Database type %s not suppported", cnf.Database.Type)
+	}
+
+	db, err := gorm.Open(cnf.Database.Type, args)
+	if err != nil {
+		return db, err
+	}
+
+	// Max idle connections
+	db.DB().SetMaxIdleConns(cnf.Database.MaxIdleConns)
+
+	// Max open connections
+	db.DB().SetMaxOpenConns(cnf.Database.MaxOpenConns)
+
+	// Database logging
+	db.LogMode(cnf.IsDevelopment)
+
+	return db, nil
 }
